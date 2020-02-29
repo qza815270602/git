4a398680fc88b77cddf26c4114361e4e4c1f97ac
qza <qza815270602@163.com>
2020-02-29 14:21:05 +0800

    init git

---
+++ src/main/java/com/dj/pms/GitApplication.java
@@ -0,0 +1,13 @@
+package com.dj.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+}
