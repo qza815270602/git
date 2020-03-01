046d2bedfde78006c7f91bd2ffd541fb41e3cc8b
qza <qza815270602@163.com>
2020-03-01 16:43:50 +0800

    提交 SellService

---
+++ src/main/java/com/dj/pms/service/SellService.java
@@ -0,0 +1,9 @@
+package com.dj.pms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dj.pms.pojo.BasicData;
+import com.dj.pms.pojo.Sell;
+
+public interface SellService extends IService<Sell> {
+
+}
